8ab66ef89c4b30a33b3ba6b1afb5b18912752da2
Sylwester Lachiewicz <slachiewicz@gmail.com>
2021-01-09 19:06:35 +0100

    Update Maven version to 3.6.3 in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MAVEN_VERSION="3.5.4"
+MAVEN_VERSION="3.6.3"
 curl -v "http://www-us.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz" -o maven.tar.gz
 tar xzf maven.tar.gz
 export MVNROOT=$PWD
