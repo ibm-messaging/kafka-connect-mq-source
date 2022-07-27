dcdf6e135539733effe701d04b3bbe9244c39dac
James MacDonald <jmacdonald@itbit.com>
2022-07-27 11:40:35 +0100

    Updated ibm-mq dependency version; updated apache URL in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 MAVEN_VERSION="3.5.4"
-curl -v "http://www-us.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz" -o maven.tar.gz
+
+#curl -v "http://www-us.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz" -o maven.tar.gz
+
+curl -vL "https://apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz" -o maven.tar.gz
+
 tar xzf maven.tar.gz
 export MVNROOT=$PWD
 export PATH=$PWD/apache-maven-${MAVEN_VERSION}/bin:$PATH
