1c11be27f948c331c6ffe38ba62d63951e1d4bba
Priyanka K U <Priyanka.K.U@ibm.com>
2023-05-18 16:17:08 +0530

    removed unwanted log

--- src/main/java/com/ibm/eventstreams/connect/mqsource/JMSReader.java
+++ src/main/java/com/ibm/eventstreams/connect/mqsource/JMSReader.java
@@ -351,7 +351,6 @@ public class JMSReader {
      * Closes the connection.
      */
     public void close() {
-        log.trace("jmscloseeeeeeee");
         log.trace("[{}] Entry {}.close", Thread.currentThread().getId(), this.getClass().getName());
 
         closeNow.set(true);
