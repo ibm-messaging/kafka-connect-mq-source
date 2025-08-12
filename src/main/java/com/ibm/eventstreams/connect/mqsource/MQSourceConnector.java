8fde8f3539a820755200979460078a43c68dfc8d
Joel Hanson <joelhanson025@gmail.com>
2025-08-12 18:24:35 +0530

    feat: Update version from 2.5.0 to 2.6.0 (#164)
    Signed-off-by: Joel Hanson <joelhanson025@gmail.com>

--- src/main/java/com/ibm/eventstreams/connect/mqsource/MQSourceConnector.java
+++ src/main/java/com/ibm/eventstreams/connect/mqsource/MQSourceConnector.java
@@ -224,7 +224,7 @@ public class MQSourceConnector extends SourceConnector {
         CONFIG_VALUE_MQ_CLIENT_RECONNECT_OPTION_DISABLED.toLowerCase(Locale.ENGLISH)
     };
 
-    public static String version = "2.5.0";
+    public static String version = "2.6.0";
 
     private Map<String, String> configProps;
 
